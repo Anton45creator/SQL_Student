046d513143b33fc627aa104c77db75a777914c54
Anton1 <loukik56@mail.ru>
2021-12-02 12:05:53 +0300

    Changed the models and fake_data files.

--- main/models.py
+++ main/models.py
@@ -1,14 +1,12 @@
-from sqlalchemy import (Column, ForeignKey, Integer, String, Table,
-                        create_engine)
+from sqlalchemy import *
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, sessionmaker
 
-DATABASE_NAME = ["student"]
+DATABASE_NAME = 'student'
 
 Base = declarative_base()
 
-engine = create_engine(f'postgresql://postgres:123456@localhost/'
-                       f'{DATABASE_NAME}')
+engine = create_engine(f'postgresql://postgres:123456@localhost/{DATABASE_NAME}')
 
 Session = sessionmaker()
 
@@ -26,32 +24,6 @@ class GroupModel(Base):
     __tablename__ = 'group'
     id = Column(Integer, primary_key=True)
     name = Column(String)
-
-    def __repr__(self) -> str:
-        return self.name
-
-
-class StudentModel(Base):
-    __tablename__ = 'student'
-    id = Column(Integer, primary_key=True)
-    group_id = Column(String)
-    first_name = Column(String)
-    last_name = Column(String)
-    courses = relationship(
-        'CourseModel',
-        secondary=association_table,
-        backref="students",
-    )
-
-    def __repr__(self) -> str:
-        return f'\n{self.first_name} {self.last_name}: {self.group_id}'
-
-
-class CourseModel(Base):
-    __tablename__ = 'course'
-    id = Column(Integer, primary_key=True)
-    name = Column(String)
-    description = Column(String)
 
     def __repr__(self) -> str:
         return self.name
